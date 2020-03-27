2a4c9f8f2978c71348299d9970079ac7627beaf7
Mario Svetec <mariosvetec@localhost.localdomain>
2020-03-27 16:42:58 +0100

    CRUDSkeletorWithMediatR

--- API/Startup.cs
+++ API/Startup.cs
@@ -12,6 +12,8 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Persistence;
 using Microsoft.EntityFrameworkCore;
+using MediatR;
+using Application.Activities;
 
 namespace API
 {
@@ -37,6 +39,7 @@ namespace API
 
                 });
             });
+            services.AddMediatR(typeof(List.Handler).Assembly);
             services.AddControllers();
         }
 
